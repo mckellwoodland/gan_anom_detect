ff22116d20d124abaa4e8e88135530e60b11b4e7
MEWoodland <mewoodland@mdanderson.org>
2024-10-21 09:41:30 -0500

    add seeds

--- scripts/generator.sh
+++ scripts/generator.sh
@@ -2,8 +2,10 @@
 
 # Set arguments here
 MODEL_PKL=""
+SEEDS=""
 OUTPUT_DIR=""
 
 stylegan2-ada-pytorch/docker_run.sh python stylegan2-ada-pytorch/generate.py \
 						--network $MODEL_PKL \
+						--seeds $SEEDS \
 						--outdir $OUTPUT_DIR
