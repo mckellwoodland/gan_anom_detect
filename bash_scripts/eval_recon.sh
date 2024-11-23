f0a12618be10fb7487180f7f0f32a0f2fd01c06f
MEWoodland <mewoodland@mdanderson.org>
2024-11-22 20:53:26 -0600

    added patch capabilities

--- bash_scripts/eval_recon.sh
+++ bash_scripts/eval_recon.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 # Required arguments.
-ORIG_DIR="/workspace/images/original/MIDRC/missing_lung/"
-RECON_DIR="/workspace/images/reconstructed/MIDRC/missing_lung/"
-OUT_PATH="/workspace/logs/dists/missing_lung_mse_full.csv"
+ORIG_DIR="/workspace/"
+RECON_DIR="/workspace/"
+OUT_PATH="/workspace/"
 
 # Optional arguments.
 DIST="MSE"
+REG="full"
+PSIZE="32"
 
 # Run script.
 docker run -it --rm -v $(pwd):/workspace gan_anom_detect python /workspace/python_scripts/eval_recon.py \
 											--orig_dir $ORIG_DIR \
 											--recon_dir $RECON_DIR \
 											--out_path $OUT_PATH \
-											--distance $DIST
+											--distance $DIST \
+											--region $REG \
+											--patch_size $PSIZE
