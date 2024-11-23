f92794cbb5d39ad12a2522b1049b38c1379a91ed
MEWoodland <mewoodland@mdanderson.org>
2024-11-22 20:56:45 -0600

    reconstruct grayscale images

--- bash_scripts/projector.sh
+++ bash_scripts/projector.sh
@@ -7,10 +7,12 @@ OUT_DIR="/workspace/"
 
 # Optional arguments.
 SAVE_VIDEO="False"
+MODE="RGB"
 
 # Run script.
 docker run --rm -v $(pwd):/workspace sg2ada python /workspace/stylegan2-ada-pytorch/projector.py \
 									--network $MODEL_PKL \
 									--target $IN_DIR \
 									--save-video $SAVE_VIDEO \
-									--outdir $OUT_DIR
+									--outdir $OUT_DIR \
+									--mode $MODE
