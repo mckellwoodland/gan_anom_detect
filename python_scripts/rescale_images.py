58fadc0bc9494a8744c9e37d954dc28da1ac0c7c
MEWoodland <mewoodland@mdanderson.org>
2025-02-11 09:27:51 -0600

    Make resolution an argument

--- python_scripts/rescale_images.py
+++ python_scripts/rescale_images.py
@@ -15,6 +15,10 @@ required = parser.add_argument_group('Required Arguments')
 required.add_argument('-i','--in_dir',type=str,required=True,help='Path to the directory containing images to be resized.')
 required.add_argument('-o','--out_dir',type=str,required=True,help='Path to the directory to put resized images into.')
 
+optional = parser.add_argument_group('Optional Arguments')
+optional.add_argument('-r','--resolution',type=int,default=512,help='Desired output resolution.\
+                                                                     Defaults to 512.')
+
 args = parser.parse_args()
 
 if not os.path.exists(args.out_dir):
@@ -26,5 +30,5 @@ if __name__=="__main__":
         out_file = os.path.join(args.out_dir, filename)
         if not os.path.exists(out_file):
             image = Image.open(os.path.join(args.in_dir, filename))
-            new_image = image.resize((512, 512))
+            new_image = image.resize((args.resolution, args.resolution))
             new_image.save(out_file)
