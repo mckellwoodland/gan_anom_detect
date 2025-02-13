97be1763bf92f6f7eac3c8ab73e1f5a1c9b89c74
MEWoodland <mewoodland@mdanderson.org>
2025-02-13 06:44:18 -0600

    remove hard paths

--- bash_scripts/slice.sh
+++ bash_scripts/slice.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Required arguments.
-IN="/workspace/images/original/computed_tomography/github_testing/cervix_nifti_windowed/"
-OUT="/workspace/images/original/computed_tomography/github_testing/cervix_nifti_windowed_masked_sliced/"
+IN="/workspace/"
+OUT="/workspace/"
 
 # Optional arguments.
-MASK="/workspace/images/original/computed_tomography/github_testing/cervix_masks/"
+MASK="/workspace/"
 
 # Run script.
 docker run --rm -v $(pwd):/workspace gan_anom_detect python python_scripts/slice.py \
