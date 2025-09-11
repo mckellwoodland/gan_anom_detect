6a9e681299af3701a75497cfdb81fe3d529cea03
MEWoodland <mewoodland@mdanderson.org>
2025-09-11 12:42:47 -0500

    add sampling script

---
+++ bash_scripts/sample_fast_ddpm.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Required arguments.
+CONFIG="Fast-DDPM/configs/liver_ct_linear.yml" # Path to the config file
+DATASET="LIVER_CT" # Name of dataset
+OUT_PTH="generative_modeling/diffusion_modeling/fast-ddpm/CT/" # Path for saving running related data
+DOC="liver_ct"
+
+# Optional arguments.
+SCHED="uniform"
+STEPS="100"
+
+# Run script.
+docker run --gpus '"device=0,1"' --rm -v $(pwd):/workspace fastddpm python /workspace/Fast-DDPM/fast_ddpm_main.py \
+                                                        --config $CONFIG \
+                                                        --dataset $DATASET \
+                                                        --exp $OUT_PTH \
+                                                        --doc $DOC \
+							--sample \
+							--fid \
+                                                        --scheduler_type $SCHED \
+                                                        --timesteps $STEPS
+
